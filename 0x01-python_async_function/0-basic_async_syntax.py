574e31044bd3a30ab90d83b852e159ee801b7139
Shedrack John <113800368+shedonyii@users.noreply.github.com>
2023-08-07 23:44:39 +0100

    Update 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
-''' Description: asynchronous coroutine that takes in an integer argument
-                 (max_delay, with a default value of 10) named wait_random
-                 that waits for a random delay between 0 and max_delay
-                 (included and float value) seconds and eventually returns it
-    Arguments: max_delay: int = 10
+'''Task 0's module.
 '''
-
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    ''' Wait up to max_delay seconds and then return length of delay. '''
-    delay = max_delay * random.random()
-    await asyncio.sleep(delay)
-    return delay
+    '''Waits for a random number of seconds.
+    '''
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
